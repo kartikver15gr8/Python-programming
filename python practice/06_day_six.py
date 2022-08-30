0a474c105aaf82df488b612e5b4ab3f571e138ad
Kartikey Verma <91731654+kartikver15gr8@users.noreply.github.com>
2022-08-30 22:14:01 +0530

    Add files via upload

---
+++ python practice/06_day_six.py
@@ -0,0 +1,24 @@
+# a = int(input("enter the value of a:\n"))
+# b = int(input("enter the value of b:\n"))
+# c = int(input("enter the value of c:\n"))
+# d = int(input("enter the value of d:\n"))
+
+# if(a>b and a>c and a>d):
+#     print("a is the greatest integer\n")
+# elif(a<b and b>c and b>d):
+#     print("b is the greatest integer\n")
+# elif(a<c and c>b and c>d):
+#     print("b is the greatest integer\n")
+# elif(d>b and d>c and a<d):
+#     print("b is the greatest integer\n")
+
+# p = int(input("enter the value of marks in physics:\n"))
+# c = int(input("enter the value of marks in chemisrty:\n"))
+# m = int(input("enter the value of marks in maths:\n"))
+
+# percent = (p+c+m)/3
+# if (p>33 and c>33 and m>33 and percent>40):
+#     print(f"The student has passed his/her examination with a percentage score of {percent},\nAnd scored {p}% in physics:\n{m}% in maths:\nAnd {c}% in chemistry!!!")
+# else:
+#     print("The student failed!")
+
