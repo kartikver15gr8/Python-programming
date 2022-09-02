e62188dc235ab2492675c496d5243c0d9c7c4b8a
Kartikey Verma <91731654+kartikver15gr8@users.noreply.github.com>
2022-09-02 11:26:02 +0530

    Add files via upload

---
+++ python practice/python practice/05_day_five.py
@@ -0,0 +1,26 @@
+# translate = {
+#             "changla manus":"good person",
+#             "bekar": "bad",
+#             "roshini": "light"}
+# print(translate["bekar"])
+# print(translate["changla manus"])
+# print(translate["roshini"])
+
+# i1=int(input("enter the value of integer1\n"))
+# i2=int(input("enter the value of integer2\n"))
+# i3=int(input("enter the value of integer3\n"))
+# i4=int(input("enter the value of integer4\n"))
+# i5=int(input("enter the value of integer5\n"))
+# i6=int(input("enter the value of integer6\n"))
+# i7=int(input("enter the value of integer7\n"))
+# i8=int(input("enter the value of integer8i8\n"))
+# num = {
+#     list : [i1,i2,i3,i4,i5,i6,i7,i8]
+# }
+# print(num[list])
+
+
+a = int(input(4))
+b = int(input(3))
+# print(a % b)
+print(f"the division of both is {a/b}")
